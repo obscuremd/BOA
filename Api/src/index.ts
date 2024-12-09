a9e3f6aa5232708f0193d774075d5e066a64be44
dan <md.erhenede@gmail.com>
2024-12-09 21:55:22 +0300

    xx

--- Api/src/index.ts
+++ Api/src/index.ts
@@ -1,55 +1,74 @@
-import express from 'express';
-import dotenv from 'dotenv'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import UserRoute from './Routes/UserRoute'
-import HistoryRoute from './Routes/HistoryRoute'
-import helmet from 'helmet';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import UserRoute from "./Routes/UserRoute";
+import HistoryRoute from "./Routes/HistoryRoute";
+import helmet from "helmet";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const mongoUrl = process?.env?.MONGO_URL
+const mongoUrl = process?.env?.MONGO_URL;
 if (!mongoUrl) {
-    throw new Error("MONGO_URL environment variable is not defined");
+  throw new Error("MONGO_URL environment variable is not defined");
 }
 
 // connect to mongoose
-mongoose.connect(mongoUrl)
-mongoose.connection.on('connected',()=>{console.log('mongoDB connection established');})
-mongoose.connection.on('error',()=>{console.log('connection error');})
+mongoose.connect(mongoUrl);
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connection established");
+});
+mongoose.connection.on("error", () => {
+  console.log("connection error");
+});
 
 // middleware
 app.use(helmet());
-app.use(cors({
+app.use(
+  cors({
     origin: (origin, callback) => {
-      if (!origin || ['https://backend-zeta-livid-99.vercel.app', 'http://localhost:5173','https://www.eaglecountybank.com', 'https://eaglecountybank.vercel.app','*'].includes(origin)) {
+      if (
+        !origin ||
+        [
+          "https://backend-zeta-livid-99.vercel.app",
+          "http://localhost:5173",
+          "https://www.eaglecountybank.com",
+          "https://eaglecountybank.vercel.app",
+          "*",
+        ].includes(origin)
+      ) {
         callback(null, true);
       } else {
-        callback(new Error('Not allowed by CORS'));
+        callback(new Error("Not allowed by CORS"));
       }
     },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }));
-app.use(express.json())
-app.use(express.static('public', {
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.json());
+app.use(
+  express.static("public", {
     setHeaders: (res, path, stat) => {
-        if (path.endsWith('.js')) {
-            res.setHeader('Content-Type', 'application/javascript');
-        }
-    }
-  }));
-
+      if (path.endsWith(".js")) {
+        res.setHeader("Content-Type", "application/javascript");
+      }
+    },
+  })
+);
 
 // Routes
-app.get('/', (req, res) => { res.send('Welcome hello worldss')})
-app.use('/user', UserRoute)
-app.use('/history', HistoryRoute)
+app.get("/", (req, res) => {
+  res.send("Welcome hello worldss");
+});
+app.use("/user", UserRoute);
+app.use("/history", HistoryRoute);
 
+app.listen(PORT, () => {
+  console.log(`listening on port http://localhost:${PORT}`);
+});
 
-app.listen(PORT, ()=> {
-    console.log(`listening on port http://localhost:${PORT}`)
-})
+export default app;
